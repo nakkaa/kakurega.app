20fb6d450685491ceabff0c7a203f89867fd3c7f
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-06-06 20:41:35 +0900

    fix: ログインボタン等が消えていた問題を修正

--- packages/frontend/src/pages/welcome.entrance.a.vue
+++ packages/frontend/src/pages/welcome.entrance.a.vue
@@ -1,7 +1,7 @@
 <template>
 <div v-if="meta" class="rsqzvsbo">
 	<MkFeaturedPhotos class="bg"/>
-	<!-- <XTimeline class="tl"/> -->
+	<XTimeline class="tl"/>
 	<div class="shape1"></div>
 	<div class="shape2"></div>
 	<img src="/client-assets/misskey.svg" class="misskey"/>
@@ -11,6 +11,18 @@
 		<MkEmoji :normal="true" :noStyle="true" emoji="😆"/>
 		<MkEmoji :normal="true" :noStyle="true" emoji="🎉"/>
 		<MkEmoji :normal="true" :noStyle="true" emoji="🍮"/>
+	</div>
+	<div class="contents">
+		<MkVisitorDashboard/>
+	</div>
+	<div v-if="instances && instances.length > 0" class="federation">
+		<MarqueeText :duration="40">
+			<MkA v-for="instance in instances" :key="instance.id" :class="$style.federationInstance" :to="`/instance-info/${instance.host}`" behavior="window">
+				<!--<MkInstanceCardMini :instance="instance"/>-->
+				<img v-if="instance.iconUrl" class="icon" :src="instance.iconUrl" alt=""/>
+				<span class="name _monospace">{{ instance.host }}</span>
+			</MkA>
+		</MarqueeText>
 	</div>
 </div>
 </template>
@@ -65,8 +77,8 @@ os.apiGet('federation/instances', {
 		width: 500px;
 		height: calc(100% - 256px);
 		overflow: hidden;
-		-webkit-mask-image: linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 128px, rgba(0, 0, 0, 1) calc(100% - 128px), rgba(0, 0, 0, 0) 100%);
-		mask-image: linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 128px, rgba(0, 0, 0, 1) calc(100% - 128px), rgba(0, 0, 0, 0) 100%);
+		-webkit-mask-image: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 128px, rgba(0,0,0,1) calc(100% - 128px), rgba(0,0,0,0) 100%);
+		mask-image: linear-gradient(0deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 128px, rgba(0,0,0,1) calc(100% - 128px), rgba(0,0,0,0) 100%);
 
 		@media (max-width: 1200px) {
 			display: none;
@@ -82,7 +94,6 @@ os.apiGet('federation/instances', {
 		background: var(--accent);
 		clip-path: polygon(0% 0%, 45% 0%, 20% 100%, 0% 100%);
 	}
-
 	> .shape2 {
 		position: fixed;
 		top: 0;
