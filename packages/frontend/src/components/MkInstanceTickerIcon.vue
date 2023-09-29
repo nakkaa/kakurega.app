4d0943a844b247ca2abcc3ca3d6095fe58bd92f6
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-09-29 19:17:42 +0900

    fix: インスタンスティッカーのアイコンが表示されないことがある問題を修正

--- packages/frontend/src/components/MkInstanceTickerIcon.vue
+++ packages/frontend/src/components/MkInstanceTickerIcon.vue
@@ -1,20 +1,25 @@
 <template>
-<div v-if="instance" v-tooltip="instance.name" :class="$style.root">
+<div v-tooltip="instance.name" :class="$style.root">
 	<img v-if="faviconUrl" :class="$style.icon" :src="faviconUrl"/>
 	<i v-if="!faviconUrl" class="ti ti-whirl"></i>
 </div>
 </template>
 
 <script lang="ts" setup>
+import * as Misskey from 'misskey-js';
+import { instanceName } from '@/config.js';
 import { instance as Instance } from '@/instance.js';
 import { getProxiedImageUrlNullable } from '@/scripts/media-proxy.js';
 
 const props = defineProps<{
-	instance?: {
-		faviconUrl?: string
-		name: string
-	}
+	instance?: Misskey.entities.User['instance'];
 }>();
+
+// if no instance data is given, this is for the local instance
+const instance = props.instance ?? {
+	name: instanceName,
+	themeColor: (document.querySelector('meta[name="theme-color-orig"]') as HTMLMetaElement).content,
+};
 
 const faviconUrl = $computed(() => props.instance ? getProxiedImageUrlNullable(props.instance.faviconUrl, 'preview') : getProxiedImageUrlNullable(Instance.iconUrl, 'preview') ?? getProxiedImageUrlNullable(Instance.iconUrl, 'preview') ?? '/favicon.ico');
 </script>
