a67a5d5773b896f9dabb886c08031193d8202d78
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-11-07 21:34:33 +0900

    enhance: 開発版の場合に初回読み込み時に最新の翻訳に更新するように

--- packages/frontend/@types/global.d.ts
+++ packages/frontend/@types/global.d.ts
@@ -10,6 +10,7 @@ declare const _VERSION_: string;
 declare const _ENV_: string;
 declare const _DEV_: boolean;
 declare const _PERF_PREFIX_: string;
+declare const _BUILT_AT_: string;
 declare const _DATA_TRANSFER_DRIVE_FILE_: string;
 declare const _DATA_TRANSFER_DRIVE_FOLDER_: string;
 declare const _DATA_TRANSFER_DECK_COLUMN_: string;
