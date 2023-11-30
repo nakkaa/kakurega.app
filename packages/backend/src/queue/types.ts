163fa577fb449c3ef7601bfde74e06fddb253508
dakkar <dakkar@thenautilus.net>
2023-11-30 13:24:57 +0000

    thread Pleroma imports as well
    I have _not_ tested this, but it should work fine, those exports are
    the same shape as Mastodon's

--- packages/backend/src/queue/types.ts
+++ packages/backend/src/queue/types.ts
@@ -55,7 +55,7 @@ export type DbJobMap = {
 	importIGToDb: DbNoteImportToDbJobData;
 	importFBToDb: DbNoteImportToDbJobData;
 	importMastoToDb: DbNoteWithParentImportToDbJobData;
-	importPleroToDb: DbNoteImportToDbJobData;
+	importPleroToDb: DbNoteWithParentImportToDbJobData;
 	importKeyNotesToDb: DbNoteWithParentImportToDbJobData;
 	importFollowing: DbUserImportJobData;
 	importFollowingToDb: DbUserImportToDbJobData;
