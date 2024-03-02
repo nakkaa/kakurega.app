923c9de5c5cb1ba2e3e9c924f5227aaa61bd7f00
yukineko <27853966+hideki0403@users.noreply.github.com>
2024-03-03 01:32:06 +0900

    feat: pgroongaを導入

--- packages/backend/migration/1703417579791-pgroonga.js
+++ packages/backend/migration/1703417579791-pgroonga.js
@@ -7,6 +7,7 @@ export class Pgroonga1703417579791 {
     name = 'Pgroonga1703417579791'
 
     async up(queryRunner) {
+			await queryRunner.query(`CREATE EXTENSION pgroonga`);
 			await queryRunner.query(`CREATE INDEX "IDX_f27f5d88941e57442be75ba9c8" ON "note" USING "pgroonga" ("text")`);
 			await queryRunner.query(`CREATE INDEX "IDX_065d4d8f3b5adb4a08841eae3c" ON "user" USING "pgroonga" ("name" pgroonga_varchar_full_text_search_ops_v2)`);
 			await queryRunner.query(`CREATE INDEX "IDX_fcb770976ff8240af5799e3ffc" ON "user_profile" USING "pgroonga" ("description" pgroonga_varchar_full_text_search_ops_v2) `);
@@ -17,5 +18,6 @@ export class Pgroonga1703417579791 {
 			await queryRunner.query(`DROP INDEX "public"."IDX_f27f5d88941e57442be75ba9c8"`);
 			await queryRunner.query(`DROP INDEX "public"."IDX_065d4d8f3b5adb4a08841eae3c"`);
 			await queryRunner.query(`DROP INDEX "public"."IDX_fcb770976ff8240af5799e3ffc"`);
+			await queryRunner.query(`DROP EXTENSION pgroonga`);
 		}
 }
